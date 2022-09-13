98a69c6973582c41dcc21fdf67fcf28be2492cd8
naimsiddiquibd <naimsiddiquibd@gmail.com>
2022-09-13 11:34:09 +0600

    2nd Updated

--- Note1.js
+++ Note1.js
@@ -1,29 +1,29 @@
-// 1. create a folder (manually or mkdir)
-// 2. open command line to that folder
-// 3. npm init -y
-// 4. npm install express (https://expressjs.com/en/starter/installing.html)
-// ----------------------------------
-// For Node Express Server
-// ----------------------------------
-// 5. Use the starter template (https://expressjs.com/en/starter/hello-world.html)
-// 6. Change port 3000 to 5000
-// 7. To run the server: node index.js
-// 8. Check browser for the port
-// 9. Change the port: process.env.PORT || 5000
-// 10. To setup auto update functionality: npm install -g nodemon (https://www.npmjs.com/package/nodemon)
-// 11. Add the "scripts" inside the "package.json":
-//     "start": "node index.js",
-//     "start-dev": "nodemon index.js",
-// 12. nodemon index.js
-// -----------------------------------
-// Get API
-// -----------------------------------
-// 13. Create API, Dynamic API, Params (https://github.com/naimsiddiquibd/DinamicAPI-Parameter-PARAMS)
-// 14. npm install cors (to use server side API to client side)
-//     require cors: const cors = require('cors');
-//     Add a middleware: app.use(cors());
-// -----------------------------------
-// Post API
-// -----------------------------------
-// 15. Create: client side, form, submit button
-// 16. 
+1. create a folder (manually or mkdir)
+2. open command line to that folder
+3. npm init -y
+4. npm install express (https://expressjs.com/en/starter/installing.html)
+----------------------------------
+For Node Express Server
+----------------------------------
+5. Use the starter template (https://expressjs.com/en/starter/hello-world.html)
+6. Change port 3000 to 5000
+7. To run the server: node index.js
+8. Check browser for the port
+9. Change the port: process.env.PORT || 5000
+10. To setup auto update functionality: npm install -g nodemon (https://www.npmjs.com/package/nodemon)
+11. Add the "scripts" inside the "package.json":
+    "start": "node index.js",
+    "start-dev": "nodemon index.js",
+12. nodemon index.js
+-----------------------------------
+Get API
+-----------------------------------
+13. Create API, Dynamic API, Params (https://github.com/naimsiddiquibd/DinamicAPI-Parameter-PARAMS)
+14. npm install cors (to use server side API to client side)
+    require cors: const cors = require('cors');
+    Add a middleware: app.use(cors());
+-----------------------------------
+Post API
+-----------------------------------
+15. Create: client side, form, submit button
+16. 
